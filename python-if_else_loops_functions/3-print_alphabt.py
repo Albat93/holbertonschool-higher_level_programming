70f7d3fcaa2e2c8e6834210d8601d655d36881b1
Albat93 <albat93120@gmail.com>
2025-01-14 10:18:51 +0100

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter != 101 and letter != 113:
+    if (chr(letter)) != 'e' and (chr(letter)) != 'q':
         print("{}".format(chr(letter)), end="")
 
