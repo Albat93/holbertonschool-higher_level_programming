58d97bbabdec63e760c7c0422fa3c1424acc8229
Albat93 <albat93120@gmail.com>
2025-01-28 12:14:16 +0100

    fix space at l32 and l33

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -29,6 +29,6 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        else :
-            for i in range (self.__size):
+        else:
+            for i in range(self.__size):
                 print("#" * self.__size)
