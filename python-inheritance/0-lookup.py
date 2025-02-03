e2ad39bf43e9b605a172f47bf000fe052154241e
Albat93 <albat93120@gmail.com>
2025-02-03 10:43:32 +0100

    initial commit with readme file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    :param obj: The object to inspect
+    :return: List of attributes and methods
+    """
+
+    return dir(obj)
