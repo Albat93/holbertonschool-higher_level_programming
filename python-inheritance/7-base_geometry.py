c5e7c652ea5b33cdf0168ce971f32c91128605e8
Albat93 <albat93120@gmail.com>
2025-02-04 17:11:25 +0100

    fix doc

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Module containing the BaseGeometry class."""
+"""Module defining the BaseGeometry class with basic geometric operations."""
 
 
 class BaseGeometry:
-    """A class with basic geometric operations."""
+    """A base class for geometric operations."""
 
     def area(self):
         """
-        Raises an Exception with the message
-        'area() is not implemented' since it is meant to be overridden.
+        Raises an Exception to indicate that the method is not implemented.
+
+        This method should be overridden in a subclass to provide
+        a specific implementation for calculating the area.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validates that 'value' is an integer and greater than 0.
+        Validates that 'value' is an integer greater than 0.
 
-        :param name: The name of the variable (as a string)
-        :param value: The value to be validated
-        :raises TypeError: If value is not an integer
-        :raises ValueError: If value is not greater than 0
+        :param name: The name of the variable (as a string).
+        :param value: The value to be validated.
+        :raises TypeError: If 'value' is not an integer.
+        :raises ValueError: If 'value' is not greater than 0.
         """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
+            raise ValueError(f"{name} must be > 0")
