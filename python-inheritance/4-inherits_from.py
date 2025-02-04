0e2c5dddfe0cadd42d5054c25350ca1414de6eae
Albat93 <albat93120@gmail.com>
2025-02-04 15:46:12 +0100

    fix for obj

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" function that returns True if the object is an instance of a class
-that inherited (directly or indirectly) from the specified class
-otherwise False.
+"""Function that checks if an object is an instance of a class
+that inherited (directly or indirectly) from the specified class.
 """
 
 
 def inherits_from(obj, a_class):
-    """check if is subclass"""
-    return issubclass(int, a_class)
+    """
+    Returns True if obj is an instance of a class that inherited
+    (directly or indirectly) from a_class. Otherwise, returns False.
+
+    :param obj: The object to check
+    :param a_class: The class to compare with
+    :return: True if obj is an instance of a subclass of a_class, else False
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
