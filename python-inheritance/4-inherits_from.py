c79221f5ca04213a7c1c47e1ce7a47acae7819b8
Albat93 <albat93120@gmail.com>
2025-02-04 15:23:01 +0100

    add doc

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class
+otherwise False.
 """
-"""
+
+
 def inherits_from(obj, a_class):
+    """check if is subclass"""
     return issubclass(int, a_class)
