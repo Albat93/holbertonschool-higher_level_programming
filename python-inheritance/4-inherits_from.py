4d163868cc6dcadd7dd738ba1c7765c6423b3c89
Albat93 <albat93120@gmail.com>
2025-02-05 09:32:56 +0100

    fix doc

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,9 +8,4 @@ def inherits_from(obj, a_class):
     """
     Returns True if obj is an instance of a class that inherited
     (directly or indirectly) from a_class. Otherwise, returns False.
-
-    :param obj: The object to check
-    :param a_class: The class to compare with
-    :return: True if obj is an instance of a subclass of a_class, else False
-    """
-    return isinstance(obj, a_class) and type(obj) != a_class
+	"""
