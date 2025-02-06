7ac5325c93c67ad796ca24be101fc82a1441d08f
Albat93 <albat93120@gmail.com>
2025-02-06 14:39:06 +0100

    fix doc

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -36,27 +36,18 @@ class Circle(Shape):
     def __init__(self, radius):
         """
         Initializes the circle with a given radius.
-
-        Parameters:
-            radius (float): The radius of the circle.
         """
         self.radius = radius
 
     def area(self):
         """
         Calculates the area of the circle.
-
-        Returns:
-            float: The area of the circle.
         """
         return math.pi * self.radius ** 2
 
     def perimeter(self):
         """
         Calculates the perimeter (circumference) of the circle.
-
-        Returns:
-            float: The perimeter of the circle.
         """
         return 2 * math.pi * self.radius
 
@@ -70,10 +61,6 @@ class Rectangle(Shape):
     def __init__(self, width, height):
         """
         Initializes the rectangle with given width and height.
-
-        Parameters:
-            width (float): The width of the rectangle.
-            height (float): The height of the rectangle.
         """
         self.width = width
         self.height = height
@@ -81,18 +68,12 @@ class Rectangle(Shape):
     def area(self):
         """
         Calculates the area of the rectangle.
-
-        Returns:
-            float: The area of the rectangle.
         """
         return self.width * self.height
 
     def perimeter(self):
         """
         Calculates the perimeter of the rectangle.
-
-        Returns:
-            float: The perimeter of the rectangle.
         """
         return 2 * (self.width + self.height)
 
@@ -100,13 +81,6 @@ class Rectangle(Shape):
 def shape_info(shape):
     """
     Prints the area and perimeter of a shape.
-
-    This function relies on duck typing, meaning it calls the 'area' and 'perimeter' methods
-    on the passed object without checking its type.
-
-    Parameters:
-        shape (Shape): An object that follows the Shape interface (i.e., has area and perimeter methods).
     """
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
-
