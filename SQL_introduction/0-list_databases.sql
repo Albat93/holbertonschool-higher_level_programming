5fc5b3dfb3a9f69839a3c2b2a498ca653b23be29
Albat93 <albat93120@gmail.com>
2025-03-03 14:34:44 +0100

    simplify MySQL database listing script by removing unnecessary bash wrapper and credentials

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-# MySQL credentials
-HOST="localhost"
-USER="root"
-PASSWORD="password"
-
-# Command to list all databases
-mysql -h$HOST -u$USER -p$PASSWORD -e "SHOW DATABASES;"
+-- lists all databases of a MySQL server.
+SHOW DATABASES;
