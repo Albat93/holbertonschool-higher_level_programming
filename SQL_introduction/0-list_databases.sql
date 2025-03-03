0caa4998aa76e39193c74ce0ac2afed26ed634bc
Albat93 <albat93120@gmail.com>
2025-03-03 14:32:28 +0100

    add script to list MySQL databases with credentials

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,9 @@
-SHOW DATABASES;
+#!/bin/bash
+
+# MySQL credentials
+HOST="localhost"
+USER="root"
+PASSWORD="password"
+
+# Command to list all databases
+mysql -h$HOST -u$USER -p$PASSWORD -e "SHOW DATABASES;"
