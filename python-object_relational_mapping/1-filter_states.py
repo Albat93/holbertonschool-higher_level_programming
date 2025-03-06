1750504f1d6b9adc7b3ca9ba012c55d5f2b653bd
Albat93 <albat93120@gmail.com>
2025-03-06 14:01:02 +0100

    refactor filter_states.py for improved readability and efficiency

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
+"""Module listing all states starting with 'N' from the database"""
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Get arguments from the command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Establish a connection to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Execute a query to select states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    # Fetch all the results
-    states = cursor.fetchall()
-
-    # Loop through and display each state in the desired format
-    for state in states:
-        print(state)
-
-    # Close the cursor and connection
-    cursor.close()
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    cur = db.cursor()
+    # Modifier la requête pour récupérer les états dont le nom commence par "N"
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
