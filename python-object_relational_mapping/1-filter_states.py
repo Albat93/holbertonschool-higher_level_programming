c3f014f1dcc320d4c248f6985b2b3e5da42b6371
Albat93 <albat93120@gmail.com>
2025-03-06 14:06:56 +0100

    fix: update SQL query to correctly list states starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module listing all states starting with 'N' from the database"""
+"""Module listing all states by N"""
 import MySQLdb
 from sys import argv
 
@@ -8,10 +8,9 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    # Modifier la requête pour récupérer les états dont le nom commence par "N"
-    query = """SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id BY ASC;"""
+    query = """SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY id ASC;"""
     cur.execute(query)
-    # Exécuter la requête
     rows = cur.fetchall()
     for row in rows:
         print(row)
